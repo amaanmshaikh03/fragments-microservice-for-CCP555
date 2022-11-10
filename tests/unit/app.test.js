24273ae04819a4bb3efe546b039f7a7301770917
amaanmshaikh03 <83049569+amaanmshaikh03@users.noreply.github.com>
2022-11-10 12:22:33 -0500

    Delete tests directory

--- tests/unit/app.test.js
+++
@@ -1,5 +0,0 @@
-const request = require('supertest');
-const app = require('../../src/app');
-describe('GET req for not found resources', () => {
-  test('404 error ', () => request(app).get('/no-such-route').expect(404));
-});
